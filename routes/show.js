b60fee201a36851066e9f9fc04dc075854f27983
Rishav Gupta <87780261+rishav-1570@users.noreply.github.com>
2022-07-16 02:07:58 +0530

    Update show.js

--- routes/show.js
+++ routes/show.js
@@ -23,7 +23,7 @@ router.get('/:uuid', async (req,res)=>{
             uuid: file.uuid,
             fileName : file.filename,
             fileSize: file.size,
-            downloadLink: `${APP_BASE_URL}/files/download/${file.uuid}`
+            downloadLink: `${process.env.APP_BASE_URL}/files/download/${file.uuid}`
             // http://localhost:3000/files/download/uuid
 
         }); 
