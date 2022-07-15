129a050a96ffdeb1789b89d11a664b939ede64ea
Rishav Gupta <87780261+rishav-1570@users.noreply.github.com>
2022-07-16 02:10:50 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,9 +2,7 @@ const path = require("path");
 require('dotenv').config(); // imorting dotenv module and call method config().
 //console.log("uu"+process.env.MONGO_CONNECTION_URL);
 // this link we got from mongodb atlas.
-const MONGO_CONNECTION_URL="mongodb+srv://file-sharing:Filesharing123@cluster0.miaytoh.mongodb.net/filedb?retryWrites=true&w=majority";
 
-const MONGO_CONNECTION_URL1="mongodb+srv://inShare:jhj6fwXZejlmhgGK@cluster0.jxt6ett.mongodb.net/inShare?retryWrites=true&w=majority";
 
 const mongoose = require('mongoose'); //import module mongoose
 function connectDB() {
